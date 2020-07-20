d826429724837f82977e05f13a29773d71a981d1
wrunion <winterrunion@gmail.com>
2020-07-19 23:37:31 -0700

    Initial commit.

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App ui container">
+      <h1>Resource Search Demo</h1>
+    </div>
+  );
+}
+
+export default App;

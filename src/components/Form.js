419ebe99d6f9f0c61deffc70263185211e198945
wrunion <winterrunion@gmail.com>
2020-08-09 12:07:45 -0700

    Form changes.

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,13 +3,45 @@ import React from 'react';
 const Form = () => {
   return (
     <div className="Form ui container segment">
-      <h3>Request for resources</h3>
-      <p>Enter the anonymous demographic data for your client:</p>
-      <p><em>All questions are optional</em></p>
-    <form>
+      {/* <h3>Request for resources</h3>
+      <p>Enter the anonymous demographic data for your client:</p> */}
+
+      {/* <p><em>All questions are optional, but the more information you provide, the more accurate your resource recommendations can be</em></p> */}
+    <form className="ui form">
+      <h3>The person looking for resources: </h3>
+      <p><em>Note: All questions are optional</em></p>
+      <div className="inline fields">
+      <div className="field">
+        <label>Lives in: </label>
+        <input type="text" placeholder="City" />
+      </div>
+      <div>- OR -</div>
+      <div className="field">
+        <input type="text" placeholder="Zip Code" />
+      </div>
+    </div>
+
+      <label>Zipcode:     
+        <div className="ui input">
+        <input type="text" />
+      </div></label><br />
+      <label>Zipcode:    
+        <div className="ui input">
+        <input type="text" />
+      </div></label><br />    <label>Zipcode:    
+        <div className="ui input">
+        <input type="text" />
+      </div></label><br />    <label>Zipcode:    
+        <div className="ui input">
+        <input type="text" />
+      </div></label><br />    <label>Zipcode:    
+        <div className="ui input">
+        <input type="text" />
+      </div></label><br />
+
       {/* Other input options here */}
-      Your email: <div className="ui input">
-        <input type="email" />
+      Contact Email: <div className="ui input">
+        <input type="email" placeholder="Email" />
       </div><br />
 
       <button className="ui button">Submit</button>
